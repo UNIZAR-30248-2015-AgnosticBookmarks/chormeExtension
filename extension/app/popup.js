331a0ae3d1db78672d2dd05f55e1374f1e44dbe1
Ramón José Arellano <ramonmoxo@gmail.com>
2016-01-15 21:18:21 +0100

    finished

--- extension/app/popup.js
+++ extension/app/popup.js
@@ -6,7 +6,7 @@ var url;
 var name;
 var userName;
 
-//Code And Encode B64
+//Code B64
 var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
 
 
@@ -41,27 +41,31 @@ function logOut(){
 	startApp();
 }
 
-
-//Use API for update/add bookmark
-function add() {
+function addBtn() {
 	if(loged){
-	
+		
+		var tags = $('#tags').val();
+		var arr = "";
+		if(!(tags === "") ){
+			arr = tags.split(",");
+		}
+		
 		var _bookmark = {
             name: $('#name').val(),
             url: $('#url').val(),
-            tags: $('#tags').val(),
+            tags: arr,
             description: $('#desc').val()
         };
-	
-		//EDIT
-		if(found){
-			$.ajaxSetup({
-			  headers : {
+		
+		$.ajaxSetup({
+			headers : {
 				'Content-Type': 'application/json',
 				'Authorization' : 'Basic ' + user
-			  }
-			});
-			
+			 }
+		});
+
+		//EDIT
+		if(found){
 		
 			$.ajax({url: 'http://adriemsworkshop.noip.me:8081/bookmarks/api/bookmarks/'+bookmarkid, data: JSON.stringify(_bookmark),
 					type: 'PATCH'})
@@ -78,13 +82,7 @@ function add() {
 		}
 		//ADD
 		else{
-			$.ajaxSetup({
-			  headers : {
-				'Content-Type': 'application/json',
-				'Authorization' : 'Basic ' + user
-			  }
-			});
-		
+			
 			$.ajax({url: 'http://adriemsworkshop.noip.me:8081/bookmarks/api/bookmarks/', data: JSON.stringify(_bookmark),
 					type: 'POST'})
 			.success(function() {
@@ -101,7 +99,6 @@ function add() {
 	}
 }
 
-//USE API for delete bookmark
 function deleteBtn() {
 	if(loged){
 	
@@ -127,8 +124,6 @@ function deleteBtn() {
 	}
 }
 
-
-//Gets the current tabInfo
 function getCurrentTabInfo(callback) {
 
     chrome.tabs.query({
@@ -154,7 +149,16 @@ function fillInfo(url,name) {
 			.success(function(json) {
 				$('#name').val(json.name);
 				$('#url').val(json.url);
-				$('#tags').val(json.tags);
+				
+				var stringTags = "";
+				for(var i in json.tags){
+					stringTags = stringTags + json.tags[i] + ",";
+				}
+				if(!(stringTags == "")){
+					stringTags = stringTags.slice(0,-1);
+				}
+				$('#tags').val(stringTags);
+				
 				$('#desc').val(json.description);
 				found = true;
 				bookmarkid = json._id;
@@ -170,8 +174,6 @@ function fillInfo(url,name) {
 				startApp();
 			})
 	}
-	
-	
 }
 
 function startApp(){	
@@ -195,22 +197,9 @@ function startApp(){
 		$('#add').html("Add");
 		$('#delete').hide();
 	}
-	
-
-
 }
 
-$("#add").click(add);
-
-$('#delete').click(deleteBtn);
-
-$("#loginBtn").click(logIn);
-
-$("#reg").click(redirect);
-
-$('#logoutBtn').click(logOut);
-
-function redirect(){
+function redirectBtn(){
 	chrome.tabs.create({url: "http://adriemsworkshop.noip.me:8081/bookmarks/#/access"});
 }
 
@@ -237,6 +226,14 @@ document.addEventListener('DOMContentLoaded', function() {
         renderStatus(errorMessage);
     });
 	
-	
-	
 });
+
+$("#add").click(addBtn);
+
+$('#delete').click(deleteBtn);
+
+$("#loginBtn").click(logIn);
+
+$("#reg").click(redirectBtn);
+
+$('#logoutBtn').click(logOut);
